c427132405116e93d1a4678c67f72aab8ce050d9
not Jon Hosler <jhosler0793@gmail.com>
2021-05-17 14:14:39 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+//Braces specify optional import
+import React, {Component} from 'react';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                React App
+            </div>
+        )
+    }
+}
+
+
+export default App;
